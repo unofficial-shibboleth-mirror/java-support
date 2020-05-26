1fefa3ea5bdb2d4f7683a59e0a33764b03579892
Rod Widdowson <rdw@steadingsoftware.com>
2020-05-26 14:53:54 +0100

    Use getter, not field.
    In this case there are potential syncrhonization issues...

--- src/main/java/net/shibboleth/utilities/java/support/component/AbstractInitializableComponent.java
+++ src/main/java/net/shibboleth/utilities/java/support/component/AbstractInitializableComponent.java
@@ -42,7 +42,7 @@ public abstract class AbstractInitializableComponent implements DestructableComp
     /** {@inheritDoc} */
     @Override
     public final synchronized void destroy() {
-        if (isDestroyed) {
+        if (isDestroyed()) {
             return;
         }
 
